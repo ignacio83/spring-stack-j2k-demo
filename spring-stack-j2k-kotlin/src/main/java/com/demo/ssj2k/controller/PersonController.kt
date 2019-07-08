99002c97cb64e43b10228c4554750b7cdaf0c6ed
Andre de Fontana Ignacio <ignacio83@gmail.com>
2019-07-08 19:23:52 -0300

    Lombok removed.

--- spring-stack-j2k-kotlin/src/main/java/com/demo/ssj2k/controller/PersonController.kt
+++ spring-stack-j2k-kotlin/src/main/java/com/demo/ssj2k/controller/PersonController.kt
@@ -1,13 +1,11 @@
 package com.demo.ssj2k.controller
 
 import com.demo.ssj2k.service.PersonService
-import lombok.RequiredArgsConstructor
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/persons")
-@RequiredArgsConstructor
 class PersonController(private val service: PersonService) {
 
     @PostMapping
